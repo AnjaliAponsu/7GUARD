826532ec8c643795265b4edea2bfc7ab1e6f747a
Dinusha Madusanka <144506606+dinusha24madusanka@users.noreply.github.com>
2024-12-10 14:51:47 +0530

    admin feedback frontend
    final admin feedback frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"; // Optional, for additional styling
+import AdminFeedback from "./components/AdminFeedback"; // Correct path and capitalization
+
+function App() {
+  return (
+    <div className="App">
+      <AdminFeedback /> {/* Render the AdminFeedback component */}
+    </div>
+  );
+}
+
+export default App;
